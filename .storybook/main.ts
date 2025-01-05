64c85d9474cdb690707f2fc833f65b2ede654a89
kzok <yes.notyet@gmail.com>
2025-01-06 00:24:37 +0900

    configure .storybook/main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,25 +1,20 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
-import { join, dirname } from "node:path";
-
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string): string {
-  return dirname(require.resolve(join(value, "package.json")));
-}
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    getAbsolutePath("@storybook/addon-onboarding"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@chromatic-com/storybook"),
-    getAbsolutePath("@storybook/addon-interactions"),
-  ],
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: "@storybook/react-vite",
     options: {},
   },
+  stories: [
+    {
+      directory: "../packages/app-wad-viewer/src/**",
+      files: "*.stories.tsx",
+    },
+  ],
+  addons: [
+    "@chromatic-com/storybook",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
 };
 export default config;
